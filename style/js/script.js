7ab2c41c78c98afee0ba30c24d79b13b4de62e5e
haikalshahab93 <haikal.pusri@gmail.com>
2023-07-13 22:17:41 +0700

    navbar

---
+++ style/js/script.js
@@ -0,0 +1,17 @@
+const navbarNav = document.querySelector('.navbar-nav');
+
+// hamburger di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+
+// klik dluar sidevbar hilangkan nav
+
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click',function(e){
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active');
+    }
+})
